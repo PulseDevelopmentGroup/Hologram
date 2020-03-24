6e1ab081fa5959798704602eccaff85ae3e3c7ae
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-03-24 18:55:30 -0400

    Fix routing

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import { Screen } from "./Screen";
+import { Admin } from "./Admin";
 
 function App() {
   return (
     <Router>
-      <Route path="/screen/:screenNumber?" component={Screen} />
+      <Switch>
+        <Route path="/play" component={Admin} />
+        <Route path="/screen/:screenNumber?" component={Screen} />
+        <Redirect to="/play" />
+      </Switch>
     </Router>
   );
 }
